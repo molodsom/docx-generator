d3e1ddb0ef748e683b60319a74003c55b4936c74
victor <victor.ryzhikov@gmail.com>
2023-04-29 15:19:32 +0100

    Make fields more compatible

--- models.py
+++ models.py
@@ -7,7 +7,7 @@ from settings import Base
 class Template(Base):
     __tablename__ = "docx_template"
     id = Column(Integer, primary_key=True)
-    file_name = Column(String)
+    file_name = Column(String(32))
     file_size = Column(Integer)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
     updated_at = Column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now())
@@ -21,10 +21,10 @@ class Field(Base):
     __tablename__ = "docx_field"
     id = Column(Integer, primary_key=True)
     template_id = Column(Integer, ForeignKey('docx_template.id'))
-    name = Column(String)
-    variable = Column(String)
+    name = Column(String(32))
+    variable = Column(String(32))
     required = Column(Boolean, default=False)
-    empty_value = Column(String, nullable=True)
+    empty_value = Column(String(255), nullable=True)
 
     def __repr__(self) -> str:
         return f"Field(id={self.id!r} template_id={self.template_id!r}, name={self.name!r}, variable={self.variable!r})"
